739e974c7305fc898f4fa0e48542a6720d2dd7bf
Thumma Bhavana <bhavanathumma3@msitprogram.net>
2021-08-29 21:27:56 +0530

    done

--- 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
+++ 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
@@ -7,5 +7,18 @@
 # so 313 is an palindrome Prime. fun_nth_palindrome_prime(0) 
 # returns 2
 
+def isprime(x):
+	if(x<2 or (x%2 == 0 and x!= 2)):
+		return False
+	for i in range(3, x//2, 2):
+		if(x%i == 0):
+			return False
+	return True
 def fun_nth_palindromic_prime(n):
-	return 0
+	f = 0
+	g = 0
+	while(f<=n):
+		g+=1
+		if(isprime(g) and str(g)==str(g)[::-1]):
+			f+=1
+	return g
